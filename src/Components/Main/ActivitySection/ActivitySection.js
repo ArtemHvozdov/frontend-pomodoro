70217ded31eb7ac58e9c8c60a8a26e4b20931a08
ihorhorbunov <igorgorbunov3333@gmail.com>
2023-06-10 20:20:22 +0300

    - add title jsx - refactor - prepare activity island

---
+++ src/Components/Main/ActivitySection/ActivitySection.js
@@ -0,0 +1,13 @@
+import TitleRow from "../../BasicComponents/TitleRow";
+import Island from "../../BasicComponents/Island";
+
+function ActivitySection() {
+
+    return (
+        <Island>
+            <TitleRow className="SnoozeSectionTittle__text" text="Activity name"></TitleRow>
+        </Island>
+    )
+}
+
+export default ActivitySection
