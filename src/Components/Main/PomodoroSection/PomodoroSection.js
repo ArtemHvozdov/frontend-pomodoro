295572d0866fa71e87eb9f3df88008dead833e2e
ArtHvozdov <artemhvozdov@gmail.com>
2023-07-03 19:30:21 +0300

    fix logic timeline section

--- src/Components/Main/PomodoroSection/PomodoroSection.js
+++ src/Components/Main/PomodoroSection/PomodoroSection.js
@@ -1,3 +1,4 @@
+import { useRef, useEffect, useState } from 'react';
 import Island from "../../BasicComponents/Island";
 import Dote from "../../BasicComponents/Dote";
 import IntervalTimeItem from "./IntervalTimeItem/IntervalTimeItem";
@@ -18,7 +19,7 @@ function PomodoroSection() {
       const hourInterval = (initialHour + hourOffset) % 24;
       const minuteInterval = (startMinute + 30 * i) % 60;
 
-      const formattedHour = hourInterval.toString().padStart(2, "0"); 
+      const formattedHour = hourInterval.toString().padStart(2, "0");
       const formattedMinute = minuteInterval.toString().padStart(2, "0");
 
       const interval = `${formattedHour}:${formattedMinute}`;
@@ -34,12 +35,44 @@ function PomodoroSection() {
       return intervalTime > currentTime;
    })
 
+   const parentContainer = useRef(null);
+   const childContainer = useRef(null);
+
+   const [scrollValue, setScrollValue] = useState(0);
+
+   useEffect(() => {
+
+      const timeLineSection = parentContainer.current;
+      const intervalTimeItem = childContainer.current;
+
+      const defaultScrollLeftValue = (timeLineSection.scrollWidth / 2) - 139;
+      setScrollValue(defaultScrollLeftValue);
+
+
+      const interval = setInterval(() => {
+         setScrollValue((prevScrollValue) => prevScrollValue + 1);
+      }, 22000)
+
+      return () => {
+         clearInterval(interval);
+      }
+   }, [])
+
+   useEffect(() => {
+      const timeLineSection = parentContainer.current;
+      timeLineSection.scrollLeft = scrollValue;
+   }, [scrollValue]);
+
    return (
       <Island>
          <div className="red-vertical-line"></div>
-         <div className="timeline__section">
+         <div className="timeline__section" ref={parentContainer}>
             {timeIntervals.map((time, index) => (
-               <div className={`timeline__section-container ${index === nearestIndex ? "nearest" : ""}`} key={index}>
+               <div
+                  className={`timeline__section-container ${index === nearestIndex ? "nearest" : ""}`}
+                  key={index}
+                  ref={childContainer}
+               >
                   <IntervalTimeItem time={time} />
                   {index < timeIntervals.length - 1 && <Dote />}
                </div>
